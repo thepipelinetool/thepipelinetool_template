96f01d60841b95bdfd7bccf2939133ca4943058e
Derrick Liu <derrick56007@hotmail.com>
2023-11-25 21:05:48 -0800

    bump ver

--- my_dag/src/main.rs
+++ my_dag/src/main.rs
@@ -1,6 +1,20 @@
-use thepipelinetool::prelude::DAG;
+use thepipelinetool::prelude::*;
 
+fn produce_data(_: ()) -> String {
+    "world".to_string()
+}
+
+fn print_data(arg: String) -> () {
+    println!("hello {arg}");
+}
+
+#[dag]
 fn main() {
-    let dag = DAG::new();
-    dag.parse_cli();
+    let opts = &TaskOptions::default();
+
+    // add a task that uses the function 'produce_data'
+    let task_ref = add_task(produce_data, (), opts);
+
+    // add a task that depends on 'task_ref'
+    let _ = add_task_with_ref(print_data, &task_ref, opts);
 }
